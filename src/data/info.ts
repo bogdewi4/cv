2bf6c0412200a2564c275850a9d01d4fbb4d522d
Aleh Bahdzevich <aleh.bogdevich@mediapark.com>
2023-01-31 09:03:12 +0300

    init

---
+++ src/data/info.ts
@@ -0,0 +1,74 @@
+export const info = {
+  baseUrl: "https://oleg-bogdevich-cv.vercel.app",
+  name: "Oleg Bogdevich",
+  jobDescription: "Web Developer",
+  about: `I'm a software engineer based in Grodno, Belarus.
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+   I have been working for 3 year. Skilled in Front-end Development.
+   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, Angular, TypeScript, React JS and Next JS,
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+  `,
+
+  experience: [
+    {
+      name: "Mediapark",
+      location: "Belarus",
+      startDate: "Aug 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Techvice",
+      location: "Belarus",
+      startDate: "Nov 2020",
+      endDate: "Aug 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "IntexSoft Software Development",
+      location: "Belarus",
+      startDate: "May 2020",
+      endDate: "Aug 2020",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Belarusian National Technical University",
+      location: "Minsk, Belarus",
+      startDate: "2016",
+      endDate: "2019",
+      description: ["The Faculty of Energy, Electrical engineering"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/bogdewi4",
+    telegram: "https://github.com/bogdewi4",
+    email: "mailto:bogdewi4.o@gmail.com",
+    linkedin: "https://www.linkedin.com/in/oleg-b-aa9791206/",
+  },
+
+  projects: [],
+};
---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface IMetaHead {
+  title: string;
+  description: string;
+  ogImageUrl: string;
+}
+
+export interface IHeroProps {
+  name: string;
+  about: string;
+}
+
+export interface IExperience {
+  name: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  description: string[];
+}
+export interface IExperiences {
+  title: string;
+  details: IExperience[];
+}
